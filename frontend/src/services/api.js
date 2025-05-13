098fad047b86f43e1d659d4b460540cc2f0894b2
EFRAIN <efrainpx99@gmail.com>
2025-05-13 01:01:29 -0400

    Apuntar a VERCEL para el frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://sublidathaapp.onrender.com/api",
 });
 
 // Request interceptor: adjunta el token en cada petición
