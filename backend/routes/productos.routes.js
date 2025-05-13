44db710a7535e1a2de58d3536bb645f3c98508e5
EFRAIN <efrainpx99@gmail.com>
2025-05-13 02:57:35 -0400

    Corrección de ultima hora al endpoint de DELETE i3

--- backend/routes/productos.routes.js
+++ backend/routes/productos.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const { Producto } = require("../models");
+const { Producto, DetallePedido } = require("../models");
 // Importar middlewares de autenticación y autorización
 const authenticateToken = require("../middlewares/auth");
 const authorizeRole = require("../middlewares/authorize");
